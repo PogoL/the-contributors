f9313591fb80c1ee0464ffa5fae35e439ee68e2c
levvy13 <mlewandowski1994@wp.pl>
2021-11-23 22:08:47 +0100

    posts list
    posts list

--- components/Post.tsx
+++ components/Post.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, Image, StyleSheet, TouchableOpacity } from 'react-native';
+import { View, Text, Image, StyleSheet, TouchableOpacity, ProgressBarAndroidComponent } from 'react-native';
 const styles = StyleSheet.create({
     tinyLogo: {
         width: '100%',
@@ -8,8 +8,7 @@ const styles = StyleSheet.create({
         position: 'absolute', zIndex: 0
     },
     rowBackground: {
-        backgroundColor: 'red',
-        marginTop: 10,
+        backgroundColor: '#2F4152',
         borderRadius: 25,
     },
     test5: {
@@ -21,40 +20,73 @@ const styles = StyleSheet.create({
         width: '100%',
         flexDirection: 'row',
         flexWrap: 'wrap',
-        // padding: 10,
     },
     rowView: {
-        // marginRight: 20,
         width:'50%',
-        padding: 20,
         paddingLeft: 0,
     },
+
+    rowView2: {
+        width:'70%',
+        justifyContent:'center',
+        borderTopRightRadius:25,
+        borderBottomRightRadius:25,
+        paddingLeft: 0,
+        backgroundColor:"#ffffff",
+        marginTop:'45%',
+        paddingTop:4,
+        paddingBottom:4,
+        alignItems: 'center',
+    },
+    rowView3: {
+        width:'70%',
+        justifyContent:'center',
+        borderTopRightRadius:25,
+        borderBottomRightRadius:25,
+        paddingLeft: 0,
+        backgroundColor:"#ffffff",
+        marginTop:'5%',
+        paddingTop:4,
+        paddingBottom:4,
+        alignItems: 'center',
+    },
+    
     
     Header: {
-        fontSize:20.23,
+        fontSize:14,
         fontWeight:'bold',
-        textAlign:'center',
-        color:'#2F4152',
-        fontFamily: 'Jost'
+        textAlign:'left',
+        color:'#ffffff',
+        lineHeight: 20.23,
+        marginLeft:10,
+        marginTop:'20%'
+
     },
     Description:{
-        fontSize:5.9,
+        fontSize:11,
+        marginLeft:10,
+        marginTop:'10%',
+        marginBottom:'10%',
+        color:'#ffffff',
+        lineHeight: 15.9,
+
     },
     testTxt1: {
-        color:"white",
-        paddingTop:'100%'
+        color:"#001EA0",
+        alignContent:'center',
+        fontSize:9,
+        fontFamily: 'jost',
+
     },
     testTxt2: {
-        color:"white",
-        paddingTop:'10%',
-        paddingBottom:'10%',
-        backgroundColor: 'pink',
-        borderTopRightRadius:25,
-        borderBottomRightRadius:25
+        color:"#2F4152",
+        alignContent:'center',
+        fontSize:9,
+        fontFamily: 'jost',
 
     },
     MainRowBackground:{
-        padding:10
+        padding:5
     }
 });
 const PostScreen = (props) => {
@@ -67,13 +99,15 @@ const PostScreen = (props) => {
                             <Image
                                 style={styles.tinyLogo}
                                 source={{
-                                    uri: 'https://reactnative.dev/img/tiny_logo.png',
+                                    uri: props.photo,
                                 }}
                             />
-                        <View style={styles.rowView}>
-                        <Text style={styles.testTxt1}>{props.name}</Text>
-                        <Text style={styles.testTxt2}>{props.name}</Text>
-                        </View>
+                            <View style={styles.rowView2}>
+                                <Text style={styles.testTxt1}>{props.howFar}</Text>
+                            </View>
+                            <View style={styles.rowView3}>
+                                <Text style={styles.testTxt2}>{props.time}</Text>
+                            </View>
 
                         </View>
                         <View style={styles.rowView}>
--- screens/PostsListScreen.tsx
+++ screens/PostsListScreen.tsx
@@ -16,9 +16,12 @@ const styles = StyleSheet.create({
     },
   });
 
-const mockDataPosts = [{Title: "Post1", Header:"Outdated queue for job offer", Description:"Do we still need to stand in this horrific concept with all this technology around?"}, 
-{Title: "Post2", Header: "More waste than food", Description: "Is it OK to serve so much single use stuff when eating inside?"},
-{Title: "Post3", Header: "Sale madness", Description: "How might we inform about sale without putting so many red signs?"}]
+const mockDataPosts = [{Photo:"https://d-nm.ppstatic.pl/kadr/k/r/5c/98/5cc17c320773b_o.jpg?",Title: "Post1", Header:"Outdated queue for job offer", Description:"Do we still need to stand in this horrific concept with all this technology around?",Time:"posted 46 minutes ago", HowFar:"100 m away from you"}, 
+{Photo:"https://codziennikmlawski.pl/wp-content/uploads/2020/11/galeria-1.jpg",Title: "Post2", Header: "More waste than food", Description: "Is it OK to serve so much single use stuff when eating inside?",Time:"posted 2 hours ago", HowFar:"4,2 km away from you"},
+{Photo:"https://cdn-thumbor.immofinanz.com/FTJ7-psQrORAl0sAoHyAlrwg4W8=/800x600/filters:quality(90)/https%3A//cdn.immofinanz.com/uploads/production/602b986b5851a4f31d39b6b0/pl_legnica_spotlight.jpg",Title: "Post3", Header: "Sale madness", Description: "How might we inform about sale without putting so many red signs?",Time:"posted 46 minutes ago", HowFar:"100 m away from you"},
+{Photo:"https://www.shop-dent.pl/data/include/cms/Zdjecia-sklepow-shop-dent/stacjonarny_sklep_shop_dent_w_Poznaniu.png",Title: "Post3", Header: "Sale madness", Description: "How might we inform about sale without putting so many red signs?",Time:"posted 46 minutes ago", HowFar:"100 m away from you"},
+{Photo:"https://retail360.pl/wp-content/uploads/2017/05/The-Body-Shop.jpg",Title: "Post3", Header: "Sale madness", Description: "How might we inform about sale without putting so many red signs?",Time:"posted 46 minutes ago", HowFar:"100 m away from you"},
+{Photo:"https://d-nm.ppstatic.pl/kadr/k/r/5c/98/5cc17c320773b_o.jpg?",Title: "Post3", Header: "Sale madness", Description: "How might we inform about sale without putting so many red signs?",Time:"posted 46 minutes ago", HowFar:"100 m away from you"}]
 
 const PostsListScreen = ({ navigation, route }: { navigation: any, route: any }) => {
 
@@ -34,21 +37,10 @@ const PostsListScreen = ({ navigation, route }: { navigation: any, route: any })
     }, [navigation]);
 
     return (
-        // <View style={styles.mainPositionView}>
-        //             <View style={styles.mainPositionView2}>
-        //     <Text style={styles.test} onPress={()=> console.log("test")}>PostsListScreen</Text>
-        // </View>
-        // <View>
-        //     <Text style={styles.test2} onPress={()=> console.log("test2")}>PostsListScreen</Text>
-        // </View>
-        // <View>
-        //     <Text style={styles.test3} onPress={()=> console.log("test3r")}>PostsListScreen</Text>
-        // </View>
-        // </View>
         <View style={styles.container} >
       <FlatList
         data={mockDataPosts}
-        renderItem={({item}) => <PostScreen name={item.Title} header={item.Header} description={item.Description}></PostScreen>}
+        renderItem={({item}) => <PostScreen photo={item.Photo} name={item.Title} header={item.Header} description={item.Description} time={item.Time} howFar={item.HowFar}></PostScreen>}
       />
     </View>
     );
